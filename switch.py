34f528619dd37910a46047f8f2946f15ea2391f4
HoriaMoraru <94983746+HoriaMoraru@users.noreply.github.com>
2023-11-21 03:29:50 +0200

    Created a special class for the Switch for modularity

--- switch.py
+++ switch.py
@@ -6,100 +6,123 @@ import threading
 import time
 from wrapper import recv_from_any_link, send_to_link, get_switch_mac, get_interface_name
 
-def parse_ethernet_header(data):
-    # Unpack the header fields from the byte array
-    #dest_mac, src_mac, ethertype = struct.unpack('!6s6sH', data[:14])
-    dest_mac = data[0:6]
-    src_mac = data[6:12]
+class Switch:
+    def __init__(self, switch_id, interfaces):
+        self.switch_id = switch_id
+        self.interfaces = interfaces
+        self.cam_table = {}  # Initialize CAM table
+        self.vlan_table = {}  # Initialize VLAN table
 
-    # Extract ethertype. Under 802.1Q, this may be the bytes from the VLAN TAG
-    ether_type = (data[12] << 8) + data[13]
+        configs_dir = "configs/"
+        config_file_name = "switch" + switch_id + ".cfg"
+        with open(configs_dir + config_file_name, "r") as config_file:
+            self.read_switch_config(config_file)
 
-    vlan_id = -1
-    # Check for VLAN tag (0x8100 in network byte order is b'\x81\x00')
-    if ether_type == 0x8200:
-        vlan_tci = int.from_bytes(data[14:16], byteorder='big')
-        vlan_id = vlan_tci & 0x0FFF  # extract the 12-bit VLAN ID
-        ether_type = (data[16] << 8) + data[17]
+        # Create and start a new thread that deals with sending BDPU
+        self.start_bdpu_thread()
 
-    return dest_mac, src_mac, ether_type, vlan_id
+    def read_switch_config(self, config_file):
+        priority = int(config_file.readline())
+        for line in config_file:
+            line = line.split()
+            interface_name = line[0]
+            interface_type = line[1]
+            self.vlan_table[interface_name] = interface_type
 
-def create_vlan_tag(vlan_id):
-    # 0x8100 for the Ethertype for 802.1Q
-    # vlan_id & 0x0FFF ensures that only the last 12 bits are used
-    return struct.pack('!H', 0x8200) + struct.pack('!H', vlan_id & 0x0FFF)
+    def start_bdpu_thread(self):
+        t = threading.Thread(target=self.send_bdpu_every_sec)
+        t.start()
 
-def send_bdpu_every_sec():
-    while True:
-        # TODO Send BDPU every second if necessary
-        time.sleep(1)
+    def send_bdpu_every_sec(self):
+        while True:
+            # TODO: Send BDPU every second if necessary
+            time.sleep(1)
 
-def is_uniscast_mac_address(mac):
-    return mac[0] & 0x01 == 0
+    def parse_ethernet_header(self, data):
+        dest_mac = data[0:6]
+        src_mac = data[6:12]
+        ether_type = (data[12] << 8) + data[13]
 
+        vlan_id = -1
+        if ether_type == 0x8200:
+            vlan_tci = int.from_bytes(data[14:16], byteorder='big')
+            vlan_id = vlan_tci & 0x0FFF
+            ether_type = (data[16] << 8) + data[17]
 
-def main():
-    # init returns the max interface number. Our interfaces
-    # are 0, 1, 2, ..., init_ret value + 1
+        return dest_mac, src_mac, ether_type, vlan_id
+
+    def create_vlan_tag(self, vlan_id):
+        return struct.pack('!H', 0x8200) + struct.pack('!H', vlan_id & 0x0FFF)
+
+    def is_unicast(self, mac_address):
+        first_byte = int(mac_address.split(":")[0], 16)
+        return (first_byte & 1) == 0
+
+    def get_interface_from_interface_name(self, interface_name):
+        for i in self.interfaces:
+            if get_interface_name(i) == interface_name:
+                return i
+
+    def main_loop(self):
+        while True:
+            interface, data, length = recv_from_any_link()
+            dest_mac, src_mac, ethertype, vlan_id = self.parse_ethernet_header(data)
+
+            dest_mac_str = ':'.join(f'{b:02x}' for b in dest_mac)
+            src_mac_str = ':'.join(f'{b:02x}' for b in src_mac)
+
+            if self.vlan_table[get_interface_name(interface)] == 'T':
+                data = data[0:12] + data[16:]
+            else:
+                vlan_id = int(self.vlan_table[get_interface_name(interface)])
+
+            send_interfaces = self.get_send_interfaces(vlan_id)
+
+            self.cam_table[src_mac_str] = interface # Update CAM table
+
+            self.handle_unicast_broadcast(interface, data, length, dest_mac_str, send_interfaces, vlan_id)
+
+    def get_send_interfaces(self, vlan_id):
+        send_interfaces = []
+
+        for interface_vlan, interface_vlan_type in self.vlan_table.items():
+            if interface_vlan_type == 'T' or int(interface_vlan_type) == vlan_id:
+                send_interfaces.append(self.get_interface_from_interface_name(interface_vlan))
+
+        return send_interfaces
+
+    def broadcast_message(self, data, length, send_interfaces, interface, vlan_id):
+        for i in send_interfaces:
+            interface_name = get_interface_name(i)
+            dest_vlan = self.vlan_table[interface_name]
+            if i != interface:
+                if dest_vlan == 'T':
+                    data = data[0:12] + self.create_vlan_tag(vlan_id) + data[12:]
+                    length += 4
+                    send_to_link(i, data, length)
+                else:
+                    send_to_link(i, data, length)
+
+    def handle_unicast_broadcast(self, interface, data, length, dest_mac_str, send_interfaces, vlan_id):
+        if self.is_unicast(dest_mac_str):
+            if dest_mac_str in self.cam_table:
+                interface_name = get_interface_name(self.cam_table[dest_mac_str])
+                dest_vlan = self.vlan_table[interface_name]
+                if dest_vlan == 'T':
+                    data = data[0:12] + self.create_vlan_tag(vlan_id) + data[12:]
+                    length += 4
+                    send_to_link(self.cam_table[dest_mac_str], data, length)
+                else:
+                    send_to_link(self.cam_table[dest_mac_str], data, length)
+            else:
+                self.broadcast_message(data, length, send_interfaces, interface, vlan_id)
+        else:
+            self.broadcast_message(data, length, send_interfaces, interface, vlan_id)
+
+if __name__ == "__main__":
     switch_id = sys.argv[1]
-
     num_interfaces = wrapper.init(sys.argv[2:])
     interfaces = range(0, num_interfaces)
 
-    print("# Starting switch with id {}".format(switch_id), flush=True)
-    print("[INFO] Switch MAC", ':'.join(f'{b:02x}' for b in get_switch_mac()))
-
-    # Create and start a new thread that deals with sending BDPU
-    t = threading.Thread(target=send_bdpu_every_sec)
-    t.start()
-
-    # Printing interface names
-    for i in interfaces:
-        print(get_interface_name(i))
-
-    while True:
-        # Note that data is of type bytes([...]).
-        # b1 = bytes([72, 101, 108, 108, 111])  # "Hello"
-        # b2 = bytes([32, 87, 111, 114, 108, 100])  # " World"
-        # b3 = b1[0:2] + b[3:4].
-        interface, data, length = recv_from_any_link()
-
-        dest_mac, src_mac, ethertype, vlan_id = parse_ethernet_header(data)
-
-        # Print the MAC src and MAC dst in human readable format
-        dest_mac = ':'.join(f'{b:02x}' for b in dest_mac)
-        src_mac = ':'.join(f'{b:02x}' for b in src_mac)
-
-        # Note. Adding a VLAN tag can be as easy as
-        # tagged_frame = data[0:12] + create_vlan_tag(10) + data[12:]
-
-        print(f'Destination MAC: {dest_mac}')
-        print(f'Source MAC: {src_mac}')
-        print(f'EtherType: {ethertype}')
-
-        print("Received frame of size {} on interface {}".format(length, interface), flush=True)
-
-        # TODO: Implement forwarding with learning
-        mac_table = {} #Initialize the mac table as an empty dictionary
-
-        mac_table[src_mac] = interface;
-        if is_uniscast_mac_address(dest_mac):
-            if dest_mac in mac_table:
-              send_to_link(mac_table[dest_mac], data, length)
-            else: #The switch does not know where to forward the frame , so it will broadcast
-              for int in interfaces:
-                if int != interface:
-                  send_to_link(int, data, length)
-        else: #The mac address is multi-cast so we forward to all interfaces
-            for int in interfaces:
-                if int != interface:
-                    send_to_link(int, data, length)
-
-        # TODO: Implement VLAN support
-        # TODO: Implement STP support
-
-        # data is of type bytes.
-        # send_to_link(i, data, length)
-
-if __name__ == "__main__":
-    main()
+    switch = Switch(switch_id, interfaces)
+    switch.main_loop()
